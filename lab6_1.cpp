8dc834c7068631f779e680ca36cf26290a330c6b
natpns <panisanat19@gmail.com>
2024-11-29 15:56:03 +0700

    finished

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int numbers = 589;
+    int evenCount = 0;
+    int oddCount = 0;
+    while (numbers != 0){
+        cout << "Enter an integer: ";
+        cin >> numbers;
+
+        if (numbers == 0) {
+            break; 
+        } 
+        if (numbers%2 == 0)
+        {
+           evenCount++;
+        }else
+           oddCount++;
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
     return 0;
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+
+
+double deg2rad(double degree){
+    return degree*M_PI/180.0;
+}
+double rad2deg(double radian){
+    return radian*180.0/M_PI;
+}
+
+double findXComponent(double length1, double length2, double angle1Rad, double angle2Rad) {
+    return length1 * cos(angle1Rad) + length2 * cos(angle2Rad);
+}
+
+
+double findYComponent(double length1, double length2, double angle1Rad, double angle2Rad) {
+    return length1 * sin(angle1Rad) + length2 * sin(angle2Rad);
+}
+
+
+double pythagoras(double xComp, double yComp) {
+    return sqrt(pow(xComp, 2) + pow(yComp, 2));
+}
+
+void showResult(double resultVecLength, double resultVecDirection) {
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << resultVecLength << endl;
+    cout << "Direction of the resultant vector (deg) = " << resultVecDirection << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
